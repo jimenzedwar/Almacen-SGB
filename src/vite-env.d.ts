e934fbdcbf82402e83b9393b9e0d57c88cee4573
jimenzedwar <jimenzeddi@gmail.com>
2024-10-01 20:13:45 -0400

    Connection with supabase and set environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+// vite-env.d.ts
+interface ImportMetaEnv {
+    readonly REACT_APP_SUPABASE_URL: string;
+    readonly REACT_APP_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
